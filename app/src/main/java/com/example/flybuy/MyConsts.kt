c16021a542e52e97c2930bd59c8e65f701834e5e
martin-ngigi <martinwainaina001@gmail.com>
2022-11-22 19:19:01 +0300

    Sign Up success using Smart-Immunization-Django API

--- app/src/main/java/com/example/flybuy/MyConsts.kt
+++ app/src/main/java/com/example/flybuy/MyConsts.kt
@@ -9,6 +9,7 @@ class MyConsts {
      * https://www.youtube.com/watch?v=BInr0629Gm8
      *
      * MY Github API link: https://github.com/martin-ngigi/Smart-Immunization-Django
+     *
      */
     companion object{
         const val BASE_URL = " https://0245-105-163-1-245.in.ngrok.io"
